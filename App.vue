865b1c85ece91078fc5f6eb671ef0f30fbba3b76
keidyya <70587056+keidyya@users.noreply.github.com>
2021-06-30 17:28:40 -0400

    main files

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <amplify-chatbot :chatbotConfig="chatbotConfig"></amplify-chatbot>
+  </div>
+</template>
+
+<script>
+
+import {Interactions} from 'aws-amplify';
+export default {
+  name: 'App',
+  data: () => ({
+    chatbotConfig:{
+      bot: "OrderFlowers_dev",
+      clearComplete: false
+    }
+  }), 
+  mounted(){
+    Interactions.onComplete("OrderFlowers_dev", this.handleComplete);
+  },
+  methods:{
+    handleComplete(err,confirmation){
+      if(err){
+        alert(err);
+        return;
+      }
+      alert(JSON.stringify(confirmation));
+    }
+  }
+
+  }
+  
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
