f6be1d1b512afff13444b8c819fb9fa2fbe907f1
keidyya <70587056+keidyya@users.noreply.github.com>
2021-07-15 15:00:31 -0400

    adding changes to make pop visible

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+const popup = document.getElementById('popup');
+const login = document.querySelector('#login_btn');
+
 
 sign_up_btn.addEventListener('click', () => {
     container.classList.add("sign-up-mode");
@@ -9,3 +12,12 @@ sign_up_btn.addEventListener('click', () => {
 sign_in_btn.addEventListener('click', () => {
     container.classList.remove("sign-up-mode");
 });
+
+function popupToggle(){
+    popup.classList.toggle('active');
+}
+
+login.addEventListener('mousedown', () => {
+    popupToggle();
+});
+    
